dcc32881ed90ae4b6f18216f93b937c0902a3d20
Francis Ferri <francisferrir@gmail.com>
2021-08-05 18:43:51 -0500

    Form with React

--- src/components/PicoPlacaForm.tsx
+++ src/components/PicoPlacaForm.tsx
@@ -1,10 +1,28 @@
+import useForm from "../hooks/useForm";
+
+interface FormData {
+	plateNumber: string;
+	date: any;
+	time: any;
+}
+
 const PicoPlacaForm = () => {
+	const { plateNumber, date, time, handleChange, form } = useForm<FormData>({
+		plateNumber: "",
+		date: "",
+		time: ""
+	});
+
+	const handleSubmit = () => {
+		alert("se dio click");
+	};
+
 	return (
 		<div className="container p-5">
 			<div className="row">
 				<div className="col-md-6">
 					<h2 className="text-center">Information</h2>
-					<form action="">
+					<form autoComplete="off">
 						<div className="form-group row">
 							<label className="col-md-4 col-lg-4 col-form-label">
 								License plate number
@@ -12,32 +30,52 @@ const PicoPlacaForm = () => {
 							<div className="col-sm-8 col-lg-8">
 								<input
 									type="text"
-									id="plate-number"
 									className="form-control"
+									name="plateNumber"
+									value={plateNumber}
+									onChange={handleChange}
 									placeholder="Write your plate number"
-									// required
 								/>
 							</div>
 						</div>
 						<div className="form-group row ">
 							<label className="col-md-4 col-lg-4 col-form-label">Date</label>
 							<div className="col-sm-8 col-lg-8">
-								<input type="date" name="" id="date" className="form-control" />
+								<input
+									type="date"
+									name="date"
+									value={date}
+									onChange={handleChange}
+									className="form-control"
+								/>
 							</div>
 						</div>
 						<div className="form-group row">
 							<label className="col-md-4 col-lg-4 col-form-label">Time</label>
 							<div className="col-sm-8 col-lg-8">
-								<input type="time" id="time" className="form-control" />
+								<input
+									type="time"
+									name="time"
+									value={time}
+									onChange={handleChange}
+									className="form-control"
+								/>
 							</div>
 						</div>
+
 						<div className="d-grid">
-							<button className="btn btn-success" type="button">
+							<button
+								className="btn btn-success"
+								type="button"
+								onClick={handleSubmit}
+							>
 								Check pico & placa
 							</button>
 						</div>
 					</form>
 				</div>
+
+				{/* Decorative Image */}
 				<div className="col-md-6">
 					<img className="image my-3" src="./assets/pico-placa.jpg" alt="" />
 				</div>
---
+++ src/hooks/useForm.ts
@@ -0,0 +1,15 @@
+import { useState, ChangeEvent } from "react";
+
+const useForm = <T extends Object>(initState: T) => {
+	const [form, setForm] = useState(initState);
+	const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = target;
+		setForm({
+			...form,
+			[name]: value
+		});
+	};
+	return { ...form, handleChange, form };
+};
+
+export default useForm;
